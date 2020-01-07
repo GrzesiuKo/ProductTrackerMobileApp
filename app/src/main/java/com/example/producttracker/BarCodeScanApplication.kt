24dc500c50130b713f6070ef09cb63edae1cc2ca
Grzegorz Kopyt <gak@autograf.pl>
2020-01-07 17:59:51 +0100

    Added first view

---
+++ app/src/main/java/com/example/producttracker/BarCodeScanApplication.kt
@@ -0,0 +1,10 @@
+package com.example.producttracker
+
+import androidx.multidex.MultiDexApplication
+
+class BarCodeScanApplication : MultiDexApplication(){
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
